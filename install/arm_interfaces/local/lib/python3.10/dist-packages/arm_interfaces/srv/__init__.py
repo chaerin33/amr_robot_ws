39b03284962c7822510b381eeb913a4260b3b74f
chaerin33 <seong610610@gmail.com>
2026-06-10 21:53:03 +0900

    Delete install directory

--- install/arm_interfaces/local/lib/python3.10/dist-packages/arm_interfaces/srv/__init__.py
+++
@@ -1,3 +0,0 @@
-from arm_interfaces.srv._arm_command import ArmCommand  # noqa: F401
-from arm_interfaces.srv._cargo import Cargo  # noqa: F401
-from arm_interfaces.srv._get_target_pose import GetTargetPose  # noqa: F401
--- install/arm_interfaces/share/arm_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(arm_interfaces_IDL_FILES "srv/ArmCommand.idl;srv/Cargo.idl;srv/GetTargetPose.idl")
-set(arm_interfaces_INTERFACE_FILES "srv/ArmCommand.srv;srv/ArmCommand_Request.msg;srv/ArmCommand_Response.msg;srv/Cargo.srv;srv/Cargo_Request.msg;srv/Cargo_Response.msg;srv/GetTargetPose.srv;srv/GetTargetPose_Request.msg;srv/GetTargetPose_Response.msg")
